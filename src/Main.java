f3f9db28d3540fa453c1f65e4bdc3913c4cbc194
Jake Dusing <dusingjake@gmail.com>
2024-12-28 21:12:49 -0600

    add burger and mealorder classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        /*Burger burger = new Burger("regular", 4.00);
+        burger.addToppings("BACON", "CHEESE", "MAYO");
+        burger.printItem();*/
+
+        /*MealOrder regularMeal = new MealOrder();
+        regularMeal.addBurgerToppings("CHEESE", "BACON", "MAYO");
+        regularMeal.setDrinkSide("LARGE");
+        regularMeal.printItemizedList();*/
+
+        MealOrder secondMeal = new MealOrder("turkey", "7-up", "chili");
+        secondMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
+        secondMeal.setDrinkSide("SMALL");
+        secondMeal.printItemizedList();
     }
 }
